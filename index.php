ef6226e0a5d659df573e17cd687c580129130eb0
csar55 <201713658b@utea.edu.pe>
2022-06-05 20:46:46 -0500

    platillla de registro index

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to you WebApp</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/stylo  .css">
+
+   
+
+  </head>
+  <body>
+    
+      <h1>Registrese</h1>
+
+      <a href="login.php">Acceso</a> or
+      <a href="registro.php">Registrate</a>
+
+  </body>
+</html>
