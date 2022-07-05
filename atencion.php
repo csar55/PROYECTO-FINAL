a0f85857eb67712cdeeb6f88717722c98c8a26ef
A7yoel <103434301+A7yoel@users.noreply.github.com>
2022-07-05 18:01:17 -0500

    Add files via upload

---
+++ atencion.php
@@ -0,0 +1,54 @@
+<?php
+//Activamos el almacenamiento en el buffer
+ob_start();
+session_start();
+
+if (!isset($_SESSION["nombre"]))
+{
+  header("Location: login.html");
+}
+else
+{
+require 'header.php';
+
+if ($_SESSION['atencion']==1)
+{
+?>
+<!--Contenido-->
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">        
+        <!-- Main content -->
+        <section class="content">
+            <div class="row">
+              <div class="col-md-12">
+                  <div class="box">
+                    <div class="box-header with-border">
+                          <h1 class="box-title">Atención <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button></h1>
+                        <div class="box-tools pull-right">
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    
+                  </div><!-- /.box -->
+              </div><!-- /.col -->
+          </div><!-- /.row -->
+      </section><!-- /.content -->
+
+    </div><!-- /.content-wrapper -->
+  <!--Fin-Contenido-->
+<?php
+}
+else
+{
+  require 'noacceso.php';
+}
+
+require 'footer.php';
+?>
+<script type="text/javascript" src="scripts/atencion.js"></script>
+<?php 
+}
+ob_end_flush();
+?>
+
+
