1dccd6c6eec0cecbcf4bbfb232eed467f689c5ae
csar55 <97277386+csar55@users.noreply.github.com>
2022-07-05 15:08:04 -0500

    Update login.php

--- login.php
+++ login.php
@@ -12,10 +12,7 @@
     <h1>Login</h1>
     <span>or <a href="registro.php">registrate</a></span>
 
-    <form action="login.php" method="post">
-    <input type ="text" name="Nombre" placeholder="ingrese su nombre">
-    <input type="password" name="password" placeholder="ingrese la contraseña">
-    <input type="submit" value="Send">  
+    
     </form>
 </body>
 </html> 
