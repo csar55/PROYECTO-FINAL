954b4e5a7371d8e22803ad6b217ac2584635ec75
csar55 <97277386+csar55@users.noreply.github.com>
2022-07-13 18:26:06 -0500

    Update atencion.php

--- atencion.php
+++ atencion.php
@@ -23,32 +23,4 @@ if ($_SESSION['atencion']==1)
               <div class="col-md-12">
                   <div class="box">
                     <div class="box-header with-border">
-                          <h1 class="box-title">Atención <button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button></h1>
-                        <div class="box-tools pull-right">
-                        </div>
-                    </div>
-                    <!-- /.box-header -->
-                    
-                  </div><!-- /.box -->
-              </div><!-- /.col -->
-          </div><!-- /.row -->
-      </section><!-- /.content -->
-
-    </div><!-- /.content-wrapper -->
-  <!--Fin-Contenido-->
-<?php
-}
-else
-{
-  require 'noacceso.php';
-}
-
-require 'footer.php';
-?>
-<script type="text/javascript" src="scripts/atencion.js"></script>
-<?php 
-}
-ob_end_flush();
-?>
-
-
+                         
